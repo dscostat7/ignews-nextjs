5fe97db90f4844ce321d1c094187049e916e23ae
Diego Souza <diegosouzagestor@gmail.com>
2022-06-03 18:57:47 -0300

    define global css and ajusts for acessibility

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from '../../node_modules/next/head';
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World!</h1>
+      <h1>Hello World!</h1>
       <p>Project for study NextJS! </p>
     </>
     
