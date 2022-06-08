1c860e00568484f4175dde4172ba36f257953175
Diego Souza <diegosouzagestor@gmail.com>
2022-06-08 18:01:21 -0300

    developing home application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,17 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
-      <p>Project for study NextJS! </p>
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl cooding" />
+      </main>
     </>
     
   )
